57e6c7de68870ee93f7df7816e8733ce5a07d573
Dennis Wachira <wachiradenis336@gmail.com>
2025-04-14 20:35:39 +0300

    style: style SearchBar component to match overall design

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 const SearchBar = ({searchTerm, setSearchTerm}) => {
   return (
-    <div style={{marginBottom: '1rem'}}>
+    <div className='search-bar'>
       <input
        type="text" 
        placeholder='Search expenses...'
        value={searchTerm}
        onChange={(e) => setSearchTerm(e.target.value)}
+       className='search-input'
      
        />
     </div>
