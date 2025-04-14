1ad9698549b34cf7e9aafd165bd1242ef99449f1
Dennis Wachira <wachiradenis336@gmail.com>
2025-04-14 18:20:31 +0300

    comment out test expenses on table

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,27 @@ import './App.css';
 
 function App() {
   const [expenses, setExpenses] = useState([
-    {
-      name: 'Spinach',
-      description: 'Groceries',
-      amount: 1200,
-      category: 'Food',
-      Date: 6/5/2024,
-    },
-    {
-      name: 'Phone',
-      description: 'Internet',
-      amount: 2500,
-      category: 'Utilities',
-      Date:6/5/2024,
-    },
-    {
-      name: 'Netflix',
-      description: 'Netflix Subscription',
-      amount: 950,
-      category: 'Entertainment',
-      Date:6/5/2024,
-    },
+    // {
+    //   name: 'Spinach',
+    //   description: 'Groceries',
+    //   amount: 1200,
+    //   category: 'Food',
+    //   Date: 6/5/2024,
+    // },
+    // {
+    //   name: 'Phone',
+    //   description: 'Internet',
+    //   amount: 2500,
+    //   category: 'Utilities',
+    //   Date:6/5/2024,
+    // },
+    // {
+    //   name: 'Netflix',
+    //   description: 'Netflix Subscription',
+    //   amount: 950,
+    //   category: 'Entertainment',
+    //   Date:6/5/2024,
+    // },
   ]);
   const handleAddExpense = (newExpense) => {
     setExpenses([newExpense, ...expenses]) //adds new expense at the top
