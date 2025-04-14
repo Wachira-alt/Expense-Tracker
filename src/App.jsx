75e97ba94955b7f6c79fb06a3f505c66d650daf8
Dennis Wachira <wachiradenis336@gmail.com>
2025-04-14 16:55:38 +0300

    feat: add an array conytaining sample expenses

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,28 @@ import ExpenseTable from '.components/ExpenseTable';
 import './app.css';
 
 function App() {
+  const sampleExpenses = [
+    {
+      description: 'Groceries',
+      amount: 1200,
+      category: 'Food',
+    },
+    {
+      description: 'Internet',
+      amount: 2500,
+      category: 'Utilities',
+    },
+    {
+      description: 'Netflix Subscription',
+      amount: 950,
+      category: 'Entertainment',
+    },
+  ];
   return (
     <>
-      <div>
+      <div className='App'>
         <h1>Expense Tracker</h1>
+        <ExpenseTable expenses = {sampleExpenses}/>
       </div>
     </>
   );
