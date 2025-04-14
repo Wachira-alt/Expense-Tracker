1b496056a7822277039afa352914c054547250c1
Dennis Wachira <wachiradenis336@gmail.com>
2025-04-14 17:57:39 +0300

    feat: Add AddExpenseForm component with form handling and state updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ExpenseTable from './components/ExpenseTable';
+import ExpenseForm from './components/ExpenseForm';
 import './App.css';
 
 function App() {
-  const sampleExpenses = [
+  const [expenses, setExpenses] = useState([
     {
+      name: 'Spinach',
       description: 'Groceries',
       amount: 1200,
       category: 'Food',
+      Date: 6/5/2024,
     },
     {
+      name: 'Phone',
       description: 'Internet',
       amount: 2500,
       category: 'Utilities',
+      Date:6/5/2024,
     },
     {
+      name: 'Netflix',
       description: 'Netflix Subscription',
       amount: 950,
       category: 'Entertainment',
+      Date:6/5/2024,
     },
-  ];
+  ]);
+  const handleAddExpense = (newExpense) => {
+    setExpenses([newExpense, ...expenses]) //adds new expense at the top
+  }
   return (
     <>
       <div className='App'>
         <h1>Expense Tracker</h1>
-        <ExpenseTable expenses = {sampleExpenses}/>
+        <ExpenseForm onAddExpense={handleAddExpense}/>
+        <ExpenseTable expenses = {expenses}/>
       </div>
     </>
   );
---
+++ src/components/ExpenseForm.jsx
@@ -0,0 +1,91 @@
+import React, {useState} from 'react';
+
+const AddExpenseForm = ({onAddExpense}) => {
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const[category, setCategory] = useState('');
+  const [amount, setAmount] = useState('');
+  const [date, setDate] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); //prevent refreshing of page
+
+    if (!name || !description ||!category || !amount ||!date  ) {
+      alert ('please fill in all the fields.')
+      return;
+    }
+    const newExpense = {
+      name,
+      description,
+      amount: parseFloat(amount),
+      category,
+      date,
+    };
+    onAddExpense(newExpense); //call parent handler
+
+    //reset fields
+    setName ('');
+    setDescription('');
+    setCategory('');
+    setAmount('');
+    setDate('');
+  };
+
+  return (
+    <form onSubmit = {handleSubmit}>
+      <h2>Add Expense</h2>
+      <div>
+        <label>Name:</label><br />
+        <input
+         type="text"
+         value={name}
+         onChange={(e) => setName(e.target.value)}
+          />
+          
+      </div>
+      <div>
+        <label>Description:</label><br />
+        <input
+         type="text"
+         value={description}
+         onChange={(e) => setDescription(e.target.value)}
+          />
+          
+      </div>
+      <div>
+        <label>Amount (Ksh):</label><br />
+        <input
+         type="number"
+         value={amount}
+         onChange={(e) => setAmount(e.target.value)}
+          />
+          
+      </div>
+      <div>
+        <label>Category:</label><br />
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="Food">Food</option>
+          <option value="Utilities">Utilities</option>
+          <option value="Entertainment">Entertainment</option>
+          <option value="Transport">Transport</option>
+        </select>
+      </div>
+      <div>
+        <label>Date:</label><br />
+        <input
+         type="date"
+         value={date}
+         onChange={(e) => setDate(e.target.value)}
+          />
+          
+      </div>
+      <button type="submit">Add Expense</button>
+     
+    </form>
+  );
+
+};
+export default AddExpenseForm;
